812bdaa3f6324bbaee5880ac1f113dd30756940f
Michael Luggen <github.michael@oiu.ch>
2017-02-14 16:31:45 +0100

    Update install.js

--- install.js
+++ install.js
@@ -7,10 +7,10 @@ var path = require('path')
 
 mkdir('-p', 'run/configuration')
 
-var config = fs.readFileSync('alod.ttl.template').toString()
+var config = fs.readFileSync('travertin.ttl.template').toString()
 
 config = config
   .split('${run}')
   .join(path.join(__dirname, 'run'))
 
-fs.writeFileSync('run/configuration/alod.ttl', config)
+fs.writeFileSync('run/configuration/travertin.ttl', config)
